ba87129cdc1e7fb1278dc282722f88b9eff39767
toxdes <hi@toxdes.com>
2026-09-18 08:48:19 +0530

    Release 3.0.2

--- Formula/vylk.rb
+++ Formula/vylk.rb
@@ -1,14 +1,14 @@
 class Vylk < Formula
   desc "Lightweight, low-resource single-binary markdown notes editor"
   homepage "https://github.com/toxdes/vylk"
-  version "3.0.1"
+  version "3.0.2"
   on_intel do
-    url "https://packages.toxdes.com/releases/vylk-3.0.1-macos-amd64.zip"
-    sha256 "8d0960cb4f5acd4368dd5bad64559bf5381c2ce98ff75325cf84a2d2e466ec4f"
+    url "https://packages.toxdes.com/releases/vylk-3.0.2-macos-amd64.zip"
+    sha256 "76b92ad53fd1a0bdf9af235cab0e98d3e32b5a126cbc56cc65a3a8cb4c09c641"
   end
   on_arm do
-    url "https://packages.toxdes.com/releases/vylk-3.0.1-macos-arm64.zip"
-    sha256 "015c4118929416a705d94e269963cac1adf0235d640464a2b5aff8e600083392"
+    url "https://packages.toxdes.com/releases/vylk-3.0.2-macos-arm64.zip"
+    sha256 "93ab76a58a2af5d5b9e9f2ae30492095e2a0e4522c773ca490e82e74415226a7"
   end
 
   def install
